897254e1bb27d6732ce5b6c908a44aa0fa5af06e
pandukakarunasena <panduka.karunasena@gmail.com>
2024-01-30 22:28:35 +0530

    Add test cases for the categoryManager class

---
+++ src/test/java/BudgetManagerTest.java
@@ -0,0 +1,42 @@
+//import org.junit.Before;
+//import org.junit.Test;
+//import services.BudgetManager;
+//
+//import static org.junit.Assert.*;
+//
+//public class BudgetManagerTest {
+//
+//
+//    private BudgetManager budgetManager;
+//
+//    @Before
+//    public void setUp() {
+//        budgetManager = new BudgetManager();
+//    }
+//
+//    @Test
+//    public void addBudget() {
+//
+//        budgetManager.addBudget(1,200);
+//    }
+//
+//    @Test
+//    public void getBudget() {
+//    }
+//
+//    @Test
+//    public void getBudgetAmount() {
+//    }
+//
+//    @Test
+//    public void setBudgetAmount() {
+//    }
+//
+//    @Test
+//    public void removeBudget() {
+//    }
+//
+//    @Test
+//    public void getBudgets() {
+//    }
+//}
