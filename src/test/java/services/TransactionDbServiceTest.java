65b76c4e61ddc878e3fc1f692ac619575244c468
pandukakarunasena <panduka.karunasena@gmail.com>
2024-01-31 20:54:37 +0530

    Generate test methods for TransactionDbService

---
+++ src/test/java/services/TransactionDbServiceTest.java
@@ -0,0 +1,64 @@
+package services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionDbServiceTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getAllTransactions() {
+    }
+
+    @Test
+    void isTransactionEmpty() {
+    }
+
+    @Test
+    void getTransactionById() {
+    }
+
+    @Test
+    void getTransactionsByAmount() {
+    }
+
+    @Test
+    void getTransactionsByType() {
+    }
+
+    @Test
+    void getTransactionsByCategoryId() {
+    }
+
+    @Test
+    void getTransactionsByNote() {
+    }
+
+    @Test
+    void getTransactionsByDate() {
+    }
+
+    @Test
+    void addAllTransactions() {
+    }
+
+    @Test
+    void addSingleTransaction() {
+    }
+
+    @Test
+    void updateTransaction() {
+    }
+
+    @Test
+    void removeTransactionByTransactionId() {
+    }
+
+    @Test
+    void removeTransactionsByCategoryId() {
+    }
+}
