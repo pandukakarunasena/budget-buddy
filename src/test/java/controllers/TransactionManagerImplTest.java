726e00bf20c3b9d4be00d1cd3fd5f825318aa732
pandukakarunasena <panduka.karunasena@gmail.com>
2024-02-01 00:12:07 +0530

    Generate test methods for controllers

---
+++ src/test/java/controllers/TransactionManagerImplTest.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionManagerImplTest {
+
+    @Test
+    void getTransactionList() {
+    }
+
+    @Test
+    void isTransactionNotEmpty() {
+    }
+
+    @Test
+    void getTransactionById() {
+    }
+
+    @Test
+    void getTransactionsByAmount() {
+    }
+
+    @Test
+    void getTransactionsByType() {
+    }
+
+    @Test
+    void getTransactionsByCategoryId() {
+    }
+
+    @Test
+    void getTransactionsByNote() {
+    }
+
+    @Test
+    void getTransactionsByDate() {
+    }
+
+    @Test
+    void isTransactionExistingByTransactionId() {
+    }
+
+    @Test
+    void isTransactionsExistingByCategoryId() {
+    }
+
+    @Test
+    void isTransactionsExistingByType() {
+    }
+
+    @Test
+    void addTransaction() {
+    }
+
+    @Test
+    void updateTransaction() {
+    }
+
+    @Test
+    void removeTransactionByTransactionId() {
+    }
+
+    @Test
+    void removeTransactionsByCategoryId() {
+    }
+
+    @Test
+    void getPrintableTransactionList() {
+    }
+}
