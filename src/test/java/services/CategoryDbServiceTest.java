ac2713dea4ed423a91da5ebff1765426b392e0a0
pandukakarunasena <panduka.karunasena@gmail.com>
2024-01-31 19:58:13 +0530

    Refactor the test classes

---
+++ src/test/java/services/CategoryDbServiceTest.java
@@ -0,0 +1,74 @@
+package services;
+
+import model.Category;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryDbServiceTest {
+
+
+    @AfterEach
+    void cleanCategoryList() {
+
+    }
+
+    @Test
+    @DisplayName("Check class type of the CategoryDbService")
+    void getInstanceCheckClassType() {
+
+    }
+
+    @Test
+    @DisplayName("Check if a single object of CategoryDBService is present")
+    void getInstanceCheckSingleton() {
+
+    }
+
+    @Test
+    @DisplayName("Get all categories when no categories added")
+    void getAllCategoriesWhenNoCategories() {
+
+    }
+
+    @Test
+    @DisplayName("Get all categories when two categories added")
+    void getAllCategoriesWhenTwoCategories() {
+
+    }
+
+    @Test
+    void isCategoryEmpty() {
+
+    }
+
+    @Test
+    void getCategoryById() {
+
+    }
+
+    @Test
+    void getCategoryIdByName() {
+
+    }
+
+    @Test
+    void addAllCategories() {
+    }
+
+    @Test
+    void addSingleCategory() {
+    }
+
+    @Test
+    void updateCategory() {
+    }
+
+    @Test
+    void removeCategoryById() {
+    }
+}
